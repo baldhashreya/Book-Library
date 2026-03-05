f29ca69f84db27d275de1bc24b1f25a1044e638a
shreya_baldha <90861621+baldhashreya@users.noreply.github.com>
2026-03-05 18:08:04 +0530

    feat: Implement initial backend architecture for user, role, authorization, book, author, and category management.

--- back_end/server/app.ts
+++ back_end/server/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { connectDB,errorHandler } from "common";
+import { connectDB, errorHandler } from "common";
 import mainRoutes from "./router/main.routes";
 import { errors } from "celebrate";
 import dotenv from "dotenv";
 import cors from "cors";
 
 dotenv.config();
-connectDB();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -16,6 +16,7 @@ app.use("/api", mainRoutes);
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+  connectDB();
+});
