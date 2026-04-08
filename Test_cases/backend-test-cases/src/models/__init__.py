946092f627f3bad589f1a74eaa51ba0e528bbaa2
shreya_baldha <90861621+baldhashreya@users.noreply.github.com>
2026-04-08 19:46:32 +0530

    refactor: remove unused legacy configuration files and associated helper utilities

--- Test_cases/backend-test-cases/src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user_model import UserModel
-from .author_model import AuthorModel
-from .book_model import BookModel
-from .category_model import CategoryModel
-from .role_model import RoleModel
