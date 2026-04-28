e1a3ccf4defaf18d2f5325d8035f1d638c6ba834
shreya_baldha <90861621+baldhashreya@users.noreply.github.com>
2026-04-28 19:53:50 +0530

    quick fix

--- back_end/server/src/controllers/authorization.controller.ts
+++ back_end/server/src/controllers/authorization.controller.ts
@@ -8,7 +8,7 @@ export class AuthorizationController {
   }
 
   public loginUser = async (req: Request, res: Response): Promise<Response> => {
-    const { password, ...logData } = req.body;
+    const { ...logData } = req.body;
     addLog(LogLevel.info, "login User", logData);
     const result = await this.authorizationServices.loginUser(req.body);
     return baseController.getResult(res, 200, result, AuthOperations.LOGIN);
